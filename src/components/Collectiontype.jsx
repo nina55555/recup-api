69370984ff2a636ebaeb5368f70057ec41ff0089
Nina <morville5audrey@gmail.com>
2024-07-15 19:49:12 +0200

    welcome effect first state scale

--- src/components/Collectiontype.jsx
+++ src/components/Collectiontype.jsx
@@ -4,12 +4,15 @@ import '../css/Collectiontype.css'
 const Collectiontype = () => {
     return (
         <div className="bigBox">
-            <div className='chooseco'>
+           
+            <div className='chooseco-choosecoG'>
                 <a className='classic-btn' href="Home">Collection classic</a>
             </div>
-            <div className="chooseco">
+            <div className="chooseco-choosecoD">
                 <a className='super-btn' href="HomeSuper">Collection super</a>
-            </div>
+            </div> 
+           
+            
         </div>
         
     );
