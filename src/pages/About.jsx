87db8dc8ff3ae26e2724a735207a6e017ea60af4
Nina <morville5audrey@gmail.com>
2024-07-28 01:44:41 +0200

    home display state 2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
             <div className="picMe">
                 <img src="" alt="portrait" />
             </div>
-            <p>Apres etre passée par la chambre syndicale de la couture parisienne et avoir travaillé en Chine, cette styliste au parcours atypique et semé d'embuches decidait de mettre en place un concept efficace qui lui tenait a coeur au travers de son art mixant d'autres passions comme la le le de et l'e</p>
+            <p>Apres etre passée par la chambre syndicale de la couture parisienne et avoir travaillé en Chine, cette styliste au parcours atypique et semé d'embuches decidait de mettre en place un concept efficace qui lui tenait a coeur au travers de son art mixant d'autres passions comme la le le depass et l'e</p>
 
         </div>
     )
