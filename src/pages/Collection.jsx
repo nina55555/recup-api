f192f8ebebff496b64097e38cb9b6e2423f1c25e
Nina <morville5audrey@gmail.com>
2026-03-02 09:52:12 +0100

    collection page slider ok and smooth but still have the last picture of the slider to arrange

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; 
 import axios from "axios";
 import { useQuery } from "react-query";
 import "../css/Collection.css";
@@ -17,6 +17,7 @@ const Collection = () => {
 
   const nextSlide = () =>
     setCurrentIndex(prev => (prev === images.length - 1 ? 0 : prev + 1));
+
   const prevSlide = () =>
     setCurrentIndex(prev => (prev === 0 ? images.length - 1 : prev - 1));
 
@@ -33,23 +34,18 @@ const Collection = () => {
       <div className="slider-3d">
         {images.map((item, index) => {
           let position = "nextSlide";
-
-          if (index === currentIndex) {
-            position = "activeSlide";
-          } else if (
+          if (index === currentIndex) position = "activeSlide";
+          else if (
             index === currentIndex - 1 ||
             (currentIndex === 0 && index === images.length - 1)
-          ) {
+          )
             position = "lastSlide";
-          }
 
           return (
             <div
               key={item._id}
-              className={`slide ${position}`}
-              style={{
-                backgroundImage: `url(${item.imageUrl})`,
-              }}
+              className={`slide ${position} ${index === currentIndex ? "transparent" : ""}`}
+              style={{ backgroundImage: `url(${item.imageUrl})` }}
             />
           );
         })}
