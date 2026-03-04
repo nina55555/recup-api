c769ccea23dbe7a8226ea3b073ac11aa421bd3c2
Nina <morville5audrey@gmail.com>
2026-03-04 06:33:59 +0100

    navbar ok a 50% carousel de collection fonctionnel peut etre amelioré ux menu deroulant ok a 95%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,112 +1,57 @@
-
-
 import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
-import "../css/Navbar.css"; //penser  a  ajouter la page css
-
-
-
+import "../css/Navbar.css";
 
 export default function Navbar() {
-  //test importé de navbar router
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuClicked, setMenuClicked] = useState(false);
+
   const handleMenuOpen = () => {
     setMenuOpen(!menuOpen);
   };
+
   const handleMenuClicked = () => {
     setMenuOpen(false);
   };
 
-
+  /* 🔥 BLOQUE SCROLL SI MENU OUVERT */
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [menuOpen]);
 
   return (
     <div className="mainBox-nav">
-      <div className="lilNav-box0">
-      </div>
+      <div className="lilNav-box0"></div>
+
       <div className="lilTitle-box1">
         <Link to="/" className="title">
-          {/*IT V*/}
-          <img src="src/assets/logoseul.png" />
-
-          {/*??? trouver logo et ecriture logo*/}
+          <img src="src/assets/logoseul.png" alt="logo" />
         </Link>
       </div>
+
       <div className="lilNav-box2">
         <ul className={menuOpen ? "open" : ""}>
-          <li>
-            <NavLink
-              to="/home"
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="collection"  
-             /*   to="Signup"*/
-              /*to= "Old"*/
-
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              Collection
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="Conc" 
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              Le Concept
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              /*  to="/Book"  */
-              to="Signup" 
-
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              Book
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              /*  to="About"  */
-              to="Signup" 
-
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              /*  to="Contact"  */
-              to="Signup" 
-
-              className={menuClicked ? "clicked" : ""}
-              onClick={handleMenuClicked}
-            >
-              Contact
-            </NavLink>
-          </li>
+          <li><NavLink to="/home" onClick={handleMenuClicked}>Home</NavLink></li>
+          <li><NavLink to="collection" onClick={handleMenuClicked}>Collection</NavLink></li>
+          <li><NavLink to="Conc" onClick={handleMenuClicked}>Le Concept</NavLink></li>
+          <li><NavLink to="Signup" onClick={handleMenuClicked}>Book</NavLink></li>
+          <li><NavLink to="Signup" onClick={handleMenuClicked}>About</NavLink></li>
+          <li><NavLink to="Signup" onClick={handleMenuClicked}>Contact</NavLink></li>
         </ul>
 
-        <div className="lilBurger-box3" onClick={handleMenuOpen}>
+        {/* 🔥 BURGER → CROIX */}
+        <div
+          className={`lilBurger-box3 ${menuOpen ? "active" : ""}`}
+          onClick={handleMenuOpen}
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
-        
       </div>
     </div>
   );
