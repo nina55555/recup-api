9ea79b5240749e625069458e1bf2605135c9350d
Nina <morville5audrey@gmail.com>
2026-03-22 13:31:01 +0100

    .env geré avec supabase et vite

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,19 +1,9 @@
+// src/lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-/*
-const supabaseUrl = "TON_URL_SUPABASE";
-const supabaseKey = "TA_PUBLIC_ANON_KEY";
-*/
+// On récupère les variables d'environnement
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-
-const supabaseUrl = "https://xfsmndahgnpsckxblfew.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhmc21uZGFoZ25wc2NreGJsZmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjg3NTQsImV4cCI6MjA4ODg0NDc1NH0.L8UFqdhrpOJq8MKNmmOel7SqzowZ5FSxRurh-3CquBQ";
-
-
-/* creer le process.env avec process.env.SUPABASE_KEY
-*/
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+// Création du client Supabase
+export const supabase = createClient(supabaseUrl, supabaseKey);
